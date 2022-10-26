364ced8ee612b30d3c8f855b55ea4e49f4e2b223
LoneWalker <ge83xoq@mytum.de>
2022-10-26 21:46:43 +0530

    -- Finished exercise sheet 1
    Signed-off-by: LoneWalker <ge83xoq@mytum.de>

--- hw01/hw01.cpp
+++ hw01/hw01.cpp
@@ -1,11 +1,14 @@
 // main function to test your work locally
 
 #include <iostream>
+#include "include/library.h"
 
 
-static_assert(__cplusplus >= 202002L);
+//static_assert(__cplusplus >= 202002L);
 
 int main() {
     int library_number = library_function();
     std::cout << library_number << std::endl;
+    std::getchar();
+    return 0;
 }
