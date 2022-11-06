f4433fd565f0317b474211bbb72d953905d7c744
LoneWalker <ge83xoq@mytum.de>
2022-11-07 00:33:19 +0530

    -Finished hw02
    Signed-off-by: LoneWalker <ge83xoq@mytum.de>

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -2,18 +2,17 @@
 
 
 uint64_t factorial(uint64_t val) {
-    // TODO
-    return 0;
+    if (val < 3)
+        return 2;
+    return val * factorial(val - 1);
 }
 
 
 uint64_t permutation(uint64_t val, uint64_t val2) {
-    // TODO
-    return 0;
+    return factorial(val)/factorial(val - val2);
 }
 
 
 uint64_t combination(uint64_t val, uint64_t val2) {
-    // TODO
-    return 0;
+    return factorial(val)/(factorial(val2) * factorial(val - val2));
 }
