f28359959dd39c1ef9a10d5caaccdac12de9054c
LoneWalker <i.am.ankur.deria@tum.de>
2022-11-17 03:47:36 +0100

    - Implemented all functions in hw03
    Signed-off-by: LoneWalker <i.am.ankur.deria@tum.de>

--- hw03/contact_list.cpp
+++ hw03/contact_list.cpp
@@ -3,5 +3,83 @@
 #include <algorithm>
 #include <numeric>
 #include <sstream>
+#include <map>
 
 // TODO create implementation here!
+using namespace contact_list;
+
+bool add(storage& contacts, std::string_view name, number_t number)
+{
+	if (name.empty())
+		return false;
+	if (number < 0)
+		return false;
+	if (std::ranges::find(contacts.names, name) != contacts.names.end())
+		return false;
+	contacts.names.push_back(static_cast<std::string>(name));
+	contacts.numbers.push_back(number);
+	return true;
+}
+
+number_t get_number_by_name(storage& contacts, std::string_view name)
+{
+	auto ret = std::find(contacts.names.begin(), contacts.names.end(), static_cast<std::string>(name));
+	if (ret != contacts.names.end())
+		return contacts.numbers[ret - contacts.names.begin()];
+	return -1;
+}
+
+std::string to_string(const storage& contacts)
+{
+	std::string result;
+	for (int i = 0; i < contacts.names.size(); i++)
+	{
+		result += contacts.names[i] + "-" + std::to_string(contacts.numbers[i]) + "\n";
+	}
+	return result;
+}
+
+bool remove(storage& contacts, std::string_view name)
+{
+	if (name.empty())
+		return false;
+	for(int i = 0; i < contacts.names.size(); i++)
+	{
+		if(name._Equal(contacts.names[i]))
+		{
+			contacts.names.erase(contacts.names.begin() + i);
+			contacts.numbers.erase(contacts.numbers.begin() + i);
+			return true;
+		}
+	}
+	return false;
+}
+
+void sort(storage& contacts)
+{
+	std::map<std::string, int> dictContacts;
+	for(int i = 0; i < contacts.names.size(); i++)
+	{
+		dictContacts.insert(std::pair<std::string, number_t>(contacts.names[i], contacts.numbers[i]));
+	}
+	contacts.names.clear();
+	contacts.numbers.clear();
+	for (const auto& tuple:dictContacts)
+	{
+		contacts.names.push_back(tuple.first);
+		contacts.numbers.push_back(tuple.second);
+	}
+}
+
+std::string get_name_by_number(storage& contacts, number_t number)
+{
+	auto ret = std::find(contacts.numbers.begin(), contacts.numbers.end(), number);
+	if (ret != contacts.numbers.end())
+		return contacts.names[ret - contacts.numbers.begin()];
+	return "";
+}
+
+size_t size(const storage& contacts)
+{
+	return contacts.names.size();
+}
