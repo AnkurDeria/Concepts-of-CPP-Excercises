e43f90a0777b041533be3e74ab3779320e8da7c1
LoneWalker <i.am.ankur.deria@tum.de>
2023-01-24 19:06:00 +0100

    - Finished hw10, test1

--- hw10/primes.h
+++ hw10/primes.h
@@ -1,3 +1,42 @@
 #pragma once
-
+#include <iostream>
 #include <cmath>
+
+template<typename T> constexpr T __gcd (T a, T b)
+{
+    return (a==0 && b!=0) ? b: __gcd(b%a, a);
+}
+
+template <typename M, typename N>
+constexpr auto gcd(const M& m, const N& n)
+{
+    return __gcd(m, n); 
+}
+
+template <typename M, typename ...Rest>
+constexpr auto gcd(const M& first, const Rest&... rest)
+{ 
+    return __gcd(first, gcd(rest...));  
+}
+
+template <typename M, typename N>
+constexpr auto mcm(const M& m, const N& n)
+{
+    return m * n /__gcd(m, n); 
+}
+
+template <typename M, typename ...Rest>
+constexpr auto mcm(const M& first, const Rest&... rest)
+{ 
+    return mcm(first, mcm(rest...));  
+}
+
+template <int base, int exponent, int modules>
+struct Power {
+    static constexpr int value = (Power<base, exponent-1, modules>::value) * base % modules;
+};
+
+template<int base, int modules>
+struct Power<base, 0, modules> {
+    static constexpr int value = 1 % modules;
+};
--- hw10/quadruple.h
+++ hw10/quadruple.h
@@ -2,11 +2,85 @@
 
 #include <array>
 
-template <typename T, typename U, typename V, typename W> struct Quadruple {
+template <typename T, typename U, typename V, typename W> 
+struct Quadruple {
 public:
+    Quadruple(T _first, U _second, V _third, W _fourth) {
+        first = _first;
+        second = _second;
+        third = _third;
+        fourth = _fourth;
+    }
 
-
+    T get_first() {
+        return first;
+    }
+    U get_second() {
+        return second;
+    }
+    V get_third() {
+        return third;
+    }
+    W get_fourth() {
+        return fourth;
+    }
 private:
     T first;
+    U second;
+    V third;
+    W fourth;
+};
+
+template<>
+struct Quadruple<int, int, int, int> {
+public:
+    Quadruple(int _first, int _second, int _third, int _fourth) {
+        members[0] = _first;
+        members[1] = _second;
+        members[2] = _third;
+        members[3] = _fourth;
+    }
+
+    int get_first() {
+        return members[0];
+    }
+    int get_second() {
+        return members[1];
+    }
+    int get_third() {
+        return members[2];
+    }
+    int get_fourth() {
+        return members[3];
+    }
+public:
+    std::array<int, 4> members;
+};
+
+
+template<typename T>
+struct Quadruple<T, T, T, T> {
+public:
+    Quadruple(T _first, T _second, T _third, T _fourth) {
+        members[0] = _first;
+        members[1] = _second;
+        members[2] = _third;
+        members[3] = _fourth;
+    }
+
+    T get_first() {
+        return members[0];
+    }
+    T get_second() {
+        return members[1];
+    }
+    T get_third() {
+        return members[2];
+    }
+    T get_fourth() {
+        return members[3];
+    }
+public:
+    std::array<T, 4> members;
 };
 
